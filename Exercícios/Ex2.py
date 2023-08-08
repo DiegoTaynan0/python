dcb63b29eff9aaf708561754f017053df4bc87d9
Diego Taynan <141446303+DiegoTaynan0@users.noreply.github.com>
2023-08-08 16:57:27 -0400

    Att 3

--- Exercícios/Ex2.py
+++ Exercícios/Ex2.py
@@ -1,27 +1,25 @@
 '''
-Imprima numeros de forma crescente de 1 a N.
-
-1. 
-valor_base
-valor_n
+1.
+fatorial
+numero
 
 2.
-Usuário deve digitar um numero aleatório, o sistema irá verificar se esse número é inteiro, se sim, ele irá imprimir número 1
-até o número que o usuário digitou, se não, irá informar que só aceita número inteiros.
+Pegar o numero digitado e multiplicar por todos números antecessores a ele.
 
-3. Não é permitido digitar números negativos ou igual ao número base.
+3.
+Não é permitido número negativos
 
-4. Que o sistema imprima os números de forma crescente.   
+4.
+Que o usuario informe o numero que deseja descobrir o fatorial e o sistema irá multiplicar por todos números que o antecedem e apresentar
+o resultado.
 '''
 
-valor_inicial = 1
-valor_n = int(input('Digite um numero: '))
-if valor_n < 1:
-    print('Digite um número inteiro!')
+fatorial = 1
+numero = int(input('Digite um número: '))
 
-elif valor_n <= valor_inicial:
-    print('Valor digitado é menor ou igual o número inicial! \n' 'Tente Novamente!!')
-
+if numero >= 1:
+    for i in range (1, numero +1):
+        fatorial = i * fatorial
+        print(fatorial)
 else:
-    for i in range (valor_inicial,valor_n):
-        print(i)
+    print('Digite um número inteiro!')
