c32df7c3351f5ddf6bfe4c7a7e149039f599453b
Diego Taynan <141446303+DiegoTaynan0@users.noreply.github.com>
2023-08-08 15:17:56 -0400

    Atualizando ex

--- Exercícios/Ex2.py
+++ Exercícios/Ex2.py
@@ -1,8 +1,27 @@
+'''
+Imprima numeros de forma crescente de 1 a N.
 
-#Ex5 - Transformar metros em centimentros
-m = int(input('Metros: '))
-cm = int(100)
+1. 
+valor_base
+valor_n
 
-valor_convertido = m * cm
+2.
+Usuário deve digitar um numero aleatório, o sistema irá verificar se esse número é inteiro, se sim, ele irá imprimir número 1
+até o número que o usuário digitou, se não, irá informar que só aceita número inteiros.
 
-print(m, 'Metro/s é igual a', valor_convertido, 'Centímetros')
+3. Não é permitido digitar números negativos ou igual ao número base.
+
+4. Que o sistema imprima os números de forma crescente.   
+'''
+
+valor_inicial = 1
+valor_n = int(input('Digite um numero: '))
+if valor_n < 1:
+    print('Digite um número inteiro!')
+
+elif valor_n <= valor_inicial:
+    print('Valor digitado é menor ou igual o número inicial! \n' 'Tente Novamente!!')
+
+else:
+    for i in range (valor_inicial,valor_n):
+        print(i)
--- Exercícios/Vários Exercícios.py
+++ Exercícios/Vários Exercícios.py
@@ -31,3 +31,31 @@ cm = int(100)
 valor_convertido = m * cm
 
 print(m, 'Metro/s é igual a', valor_convertido, 'Centímetros')
+
+'''
+Imprima numeros de forma crescente de 1 a N.
+
+1. 
+valor_base
+valor_n
+
+2.
+Usuário deve digitar um numero aleatório, o sistema irá verificar se esse número é inteiro, se sim, ele irá imprimir número 1
+até o número que o usuário digitou, se não, irá informar que só aceita número inteiros.
+
+3. Não é permitido digitar números negativos ou igual ao número base.
+
+4. Que o sistema imprima os números de forma crescente.   
+'''
+
+valor_inicial = 1
+valor_n = int(input('Digite um numero: '))
+if valor_n < 1:
+    print('Digite um número inteiro!')
+
+elif valor_n <= valor_inicial:
+    print('Valor digitado é menor ou igual o número inicial! \n' 'Tente Novamente!!')
+
+else:
+    for i in range (valor_inicial,valor_n):
+        print(i)
